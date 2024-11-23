317f84ec22e5a4276719822b28d557a746bbc026
vali1014 <dunai.valeria@gmail.com>
2024-11-23 23:21:17 +0100

    létrehoztam a menüt, az almenüket és formáztam

---
+++ templates/regisztracio.php
@@ -0,0 +1,11 @@
+<?php
+$title = "Regisztráció";
+include __DIR__ . '/header.php';
+?>
+
+<div class="container">
+    <h1>Regisztráció</h1>
+    <p>Regisztrációs űrlap.</p>
+</div>
+
+<?php include __DIR__ . '/footer.php'; ?>
---
+++ templates/szelenergia.php
@@ -0,0 +1,11 @@
+<?php
+$title = "Szélenergia";
+include __DIR__ . '/header.php';
+?>
+
+<div class="container">
+    <h1>Szélenergia</h1>
+    <p>Információk a szélenergia hasznosításáról.</p>
+</div>
+
+<?php include __DIR__ . '/footer.php'; ?>
